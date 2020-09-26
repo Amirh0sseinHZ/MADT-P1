1384995e2fb940ca96928d1010e6bf62ed471cf6
k1lleR8 <amirziaee12@gmail.com>
2020-09-26 20:15:59 +0300

    Added a text and button to change the text.

--- app/src/main/java/com/example/madt_p1/MainActivity.java
+++ app/src/main/java/com/example/madt_p1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.madt_p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvWelcomeMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvWelcomeMsg = findViewById(R.id.twWelcomeMsg);
+    }
+
+    public void onBtnChangeTxtClick(View view) {
+        tvWelcomeMsg.setText("Goodbye!");
     }
 }
